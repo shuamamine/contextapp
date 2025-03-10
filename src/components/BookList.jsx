c87d0eadbdb441c469a78340abe6db7b7afe90cc
shuamamine <sahasenjuti796@gmail.com>
2025-03-11 02:53:45 +0530

    setup

---
+++ src/components/BookList.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+class BookList extends Component {
+ render(){
+    return (
+        <>
+        <div class="booklist">
+          <ul>
+            <li>the way of kings</li>
+            <li>the name of the wind</li>
+            <li>the final empire</li>
+          </ul>
+        </div>
+        </>
+    );
+ } 
+    
+};
+
+export default BookList;
